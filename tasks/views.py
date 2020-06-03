2e089ac557f5930e3f466c5860d562e2155e04ea
zelendanny <zelen.danny@gmail.com>
2020-06-04 00:59:39 +0300

    crud api

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tasks to be viewed or edited.
+    """
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
