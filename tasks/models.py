ddbf80fcaa14834fbd45f903494838e3ba85e876
zelendanny <zelen.danny@gmail.com>
2020-06-04 00:55:07 +0300

    task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=30, default='')
+    description = models.CharField(max_length=300, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline_at = models.DateTimeField(null=True)
+    is_completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True)
