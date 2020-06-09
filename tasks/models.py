0bed029007c9e09fe52453a8989083f6298d6138
zelendanny <zelen.danny@gmail.com>
2020-06-09 13:03:48 +0300

    manual set up created time on save

--- tasks/models.py
+++ tasks/models.py
@@ -5,11 +5,12 @@ from datetime import datetime
 class Task(models.Model):
     title = models.CharField(max_length=30, default='')
     description = models.CharField(max_length=300, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(null=True)
     deadline_at = models.DateTimeField(null=True)
     is_completed = models.BooleanField(default=False)
     completed_at = models.DateTimeField(null=True)
 
     def save(self, **kwargs):
+        self.created_at = self.created_at or datetime.now()
         self.completed_at = datetime.now() if self.is_completed else None
         super().save(**kwargs)
